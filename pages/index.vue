64df4117b55e27b2069282b939bdbf369396b380
nicodona99 <nicodona99@gmail.com>
2023-05-18 18:31:55 +0200

    prova

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
     <main>
         <img id = "main-img" src = "~/assets/img/home-image.jpg" />
         <h2>
-            This is a description for the Dog's Hotel. The best place where dogs can rest.
+            This is a description for the Dog's Hotel. The best place where dogs can rest...
         </h2>
     </main>
 </template>
